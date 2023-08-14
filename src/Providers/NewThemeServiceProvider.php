d70b79bdd345dacfbd20d203e232f4c7f51b83a1
doproiescu-plenty <126688988+doproiescu-plenty@users.noreply.github.com>
2023-08-14 12:53:27 +0300

    Testing new theme provider

---
+++ src/Providers/NewThemeServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+namespace NewTheme\Providers;
+
+use IO\Helper\TemplateContainer;
+use Plenty\Plugin\Events\Dispatcher;
+use Plenty\Plugin\ServiceProvider;
+use Plenty\Plugin\Templates\Twig;
+
+/**
+ * Class HelloWorldServiceProvider
+ * @package HelloWorld\Providers
+ */
+class NewThemeServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register the service provider.
+     */
+    public function register()
+    {
+        $this->getApplication()->register(HelloWorldRouteServiceProvider::class);
+    }
+
+    /**
+     * Boot a template for the basket that will be displayed in the template plugin instead of the original basket.
+     */
+    public function boot(Twig $twig, Dispatcher $eventDispatcher)
+    {
+        $eventDispatcher->listen('IO.tpl.basket', function(TemplateContainer $container)
+        {
+            $container->setTemplate('Theme::content.ThemeSection');
+            return false;
+        }, 0);
+    }
+}
