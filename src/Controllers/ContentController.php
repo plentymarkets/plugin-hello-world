821cfc665582c58197233de8b88d7210fe8f590e
smitrea-plenty <stefan.mitrea@plentyone.com>
2025-11-06 16:41:33 +0200

    atomic test 36

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -114,3 +114,4 @@ class ContentController extends Controller
 // atomic test 33 Thu Nov  6 16:41:19 EET 2025
 // atomic test 34 Thu Nov  6 16:41:24 EET 2025
 // atomic test 35 Thu Nov  6 16:41:28 EET 2025
+// atomic test 36 Thu Nov  6 16:41:33 EET 2025
