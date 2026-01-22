e5e04ee2dd7ed71b8dc9628032851435120609af
ovivan-plenty <121545090+ovivan-plenty@users.noreply.github.com>
2026-01-22 09:30:24 +0200

    Update HelloWorldRouteServiceProvider.php

--- src/Providers/HelloWorldRouteServiceProvider.php
+++ src/Providers/HelloWorldRouteServiceProvider.php
@@ -15,7 +15,6 @@ class HelloWorldRouteServiceProvider extends RouteServiceProvider
 	 */
 	public function map(Router $router)
 	{
-		$router->get('hello', 'HelloWorld\Controllers\ContentController@sayHello');
 		$router->get('hello', 'HelloWorld\Controllers\DeprecatedShowcaseController@demo');
 	}
 
