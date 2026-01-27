929ecac37880bedbd89fecc4d469ed398a741adf
ovivan-plenty <121545090+ovivan-plenty@users.noreply.github.com>
2026-01-27 12:16:58 +0200

    Update HelloWorldServiceProvider.php

--- src/Providers/HelloWorldServiceProvider.php
+++ src/Providers/HelloWorldServiceProvider.php
@@ -2,6 +2,8 @@
 namespace HelloWorld\Providers;
 
 use Plenty\Plugin\ServiceProvider;
+use Exception;
+use Illuminate\Contracts\Foundation\Application;
 
 /**
  * Class HelloWorldServiceProvider
@@ -16,5 +18,9 @@ class HelloWorldServiceProvider extends ServiceProvider
 	public function register()
 	{
 		$this->getApplication()->register(HelloWorldRouteServiceProvider::class);
+	}
+
+	public function bootstrap(Application $app) {
+		throw new Exception('Should not boot');
 	}
 }
