7cbb68ad8304201b8d3e60b58f4de90ba2afb0e7
ovivan-plenty <ovidiu.ivan@plentymarkets.com>
2026-01-27 14:52:44 +0200

    Add incompatible code

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,7 +1,16 @@
 <?php
 
-namespace HelloWorld;
-
-readonly class BreaksIn82 {
-    public $value;
+$encoded = utf8_encode("Ștefan");
+if ($encoded === false) {
+    throw new RuntimeException("utf8_encode failed");
 }
+
+$decoded = utf8_decode($encoded);
+if ($decoded === false) {
+    throw new RuntimeException("utf8_decode failed");
+}
+
+$name = "Eve";
+$badInterpolation = "Hello ${name}"; // triggers deprecation
+
+$oldDate = strftime('%Y-%m-%d'); // triggers deprecation
