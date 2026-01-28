0782b4e2dcc32791404095a11a0173776a8578a3
ovivan-plenty <ovidiu.ivan@plentymarkets.com>
2026-01-28 12:12:46 +0200

    Add incompatible code

--- src/Controllers/DeprecatedShowcaseController.php
+++ src/Controllers/DeprecatedShowcaseController.php
@@ -9,7 +9,6 @@ class DeprecatedShowcaseController extends Controller
 {
     public function demo(Twig $twig): string
     {
-        app( \HelloWorld\Controllers\HelloWorld\Controllers\TestClass::class)->run();
         $array = [1, 2, 3];
         each($array);
 
