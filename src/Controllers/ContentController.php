bacb887d8dabf9188b752b3e001f2d1460a57a14
smitrea-plenty <stefan.mitrea@plentyone.com>
2026-05-07 10:40:42 +0300

    Test plenty carbon

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -3,6 +3,7 @@ namespace HelloWorld\Controllers;
 
 use Plenty\Plugin\Controller;
 use Plenty\Plugin\Templates\Twig;
+use Plenty\Carbon;
 
 /**
  * Class ContentController
@@ -16,6 +17,25 @@ class ContentController extends Controller
 	 */
 	public function sayHello(Twig $twig):string
 	{
-		return $twig->render('HelloWorld::content.hello');
+		$past = Carbon::parse('2020-01-01 00:00:00');
+		$future = Carbon::parse('2026-05-07 10:00:00');
+
+		// Both should be positive (absolute) regardless of order
+		$pastToFuture = $past->diffInSeconds($future);
+		$futureToPast = $future->diffInSeconds($past);
+
+		$pastToFutureMinutes = $past->diffInMinutes($future);
+		$futureToPastMinutes = $future->diffInMinutes($past);
+
+		// With $absolute = false, should return negative
+		$signed = $future->diffInSeconds($past, false);
+
+		return $twig->render('HelloWorld::content.hello', [
+			'pastToFuture' => $pastToFuture,
+			'futureToPast' => $futureToPast,
+			'pastToFutureMinutes' => $pastToFutureMinutes,
+			'futureToPastMinutes' => $futureToPastMinutes,
+			'signed' => $signed,
+		]);
 	}
 }
